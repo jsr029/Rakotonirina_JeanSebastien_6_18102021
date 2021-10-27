0248244f52dd69fa9f8c26d3a7c73a4413718eec
jsr029 <jsr029@gmail.com>
2021-10-28 01:13:44 +0200

    Updatr

--- js/launchIndex.js
+++ js/launchIndex.js
@@ -1,49 +1,48 @@
 //var header = document.querySelector('header');
 var section = document.querySelector('section');
 function launchIndex(datasPhs) {
-    var fotografers = datasPhs;
-    fotografers = Array.from(new Set(fotografers));
-    console.log(fotografers);
-    for (var i = 0; i < fotografers.length; i++) {
-        var myArticle = document.createElement('article');
-        myArticle.className = "ph";
-        myArticle.label = fotografers[i].name + "'s details";
-        var myH2 = document.createElement('h2');
-        myH2.className = "name";
-        var myPara1 = document.createElement('img');
-        myPara1.className = "foto";
-        var myPara2 = document.createElement('div');
-        myPara2.className = "location";
-        var myPara3 = document.createElement('div');
-        myPara3.className = "tagline";
-        var myPara4 = document.createElement('div');
-        myPara4.className = "price";
-        var myList = document.createElement('ul');
-        myList.className = "tags";
+        var fotografers = datasPhs;
+        fotografers = Array.from(new Set(fotografers));
+        console.log(fotografers);
+        for (var i = 0; i < fotografers.length; i++) {
+            var myArticle = document.createElement('article');
+            myArticle.className = "ph";
+            myArticle.label = fotografers[i].name + "'s details";
+            var myH2 = document.createElement('h2');
+            myH2.className = "name";
+            var myPara1 = document.createElement('img');
+            myPara1.className = "foto";
+            var myPara2 = document.createElement('div');
+            myPara2.className = "location";
+            var myPara3 = document.createElement('div');
+            myPara3.className = "tagline";
+            var myPara4 = document.createElement('div');
+            myPara4.className = "price";
+            var myList = document.createElement('ul');
+            myList.className = "tags";
 
-        var phGphTags = [];
-        myH2.textContent = fotografers[i].name;
-        myPara1.src = './img/Photographers ID Photos/' + fotografers[i].portrait;
-        myPara2.textContent = fotografers[i].city + ', ' + fotografers[i].country;
-        myPara3.textContent = fotografers[i].tagline;
-        myPara4.textContent = fotografers[i].price + '€/jour';
-        phGphTags = fotografers[i].tags;
-        for (var j = 0; j < phGphTags.length; j++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = '#' + phGphTags[j];
-        myList.appendChild(listItem);
+            var phGphTags = [];
+            myH2.textContent = fotografers[i].name;
+            myPara1.src = './img/Photographers ID Photos/' + fotografers[i].portrait;
+            myPara2.textContent = fotografers[i].city + ', ' + fotografers[i].country;
+            myPara3.textContent = fotografers[i].tagline;
+            myPara4.textContent = fotografers[i].price + '€/jour';
+            phGphTags = fotografers[i].tags;
+            for (var j = 0; j < phGphTags.length; j++) {
+            var listItem = document.createElement('li');
+            listItem.textContent = '#' + phGphTags[j];
+            myList.appendChild(listItem);
+            }
+
+            myArticle.appendChild(myPara1);
+            myArticle.appendChild(myH2);
+            myArticle.appendChild(myPara2);
+            myArticle.appendChild(myPara3);
+            myArticle.appendChild(myPara4);      
+            myArticle.appendChild(myList);
+
+            section.appendChild(myArticle);
         }
-
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-        myArticle.appendChild(myPara4);      
-        myArticle.appendChild(myList);
-
-        section.appendChild(myArticle);
-    }
-
 }
 
 export default launchIndex;
--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 import databaseAvailable from "./json2Js.js";
 import launchIndex from "./launchIndex.js";
+var d = [];
+var datasPhs = [];
+const aList = document.querySelectorAll('a');
 databaseAvailable.then(function(database) {
-        var d = database.photographers;
-        const aList = document.querySelectorAll('a');
+        d = database.photographers;
         launchIndex(d);
-        var datasPhs = [];
         //console.log(d);
         aList.forEach((btn) => btn.addEventListener("click", getTagUrl));
-        function getTagUrl(event){
-            const urlClicked = event.target.toString();
-            const urlSplit = urlClicked.split('#');
-            const urlTag = urlSplit[1];
-            console.log(urlTag);
-            d.forEach(function(k){
-            if(k.tags.includes(urlTag)){
-                datasPhs = [];
-                datasPhs.push(k);
-                console.log(k);
-                launchIndex(datasPhs);
-                    }
-                });
-        }
     console.log(database);
 });
+function getTagUrl(){
+    const urlClicked = window.location.href;
+    const urlSplit = urlClicked.split('#');
+    const urlTag = urlSplit[1];
+    console.log(urlTag);
+        d.forEach(function(k){
+             if(k.tags.includes(urlTag)){
+            datasPhs = [];
+            datasPhs.push(k);
+            console.log(k);
+            launchIndex(datasPhs);
+                }
+            });
+}
+function removeListPhs(){
+     // select the target element
+     const e = document.querySelectorAll("article");
+     // remove the list item
+     for(let i=0; i < e.length; i++){
+        e[i].parentElement.removeChild(e[i]);
+     }
+}
+window.addEventListener('hashchange', function() {
+    removeListPhs();
+    getTagUrl();
+  }, false);
+  
