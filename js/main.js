4f9b7fed98655e8fea8c7768c70603f248979efd
jsr029 <jsr029@gmail.com>
2021-11-11 16:41:47 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
  *  3. block main
  *    a. base photographers récupérer portrait, name, address (city,country), tagline, tags, price 
 */
-import databaseAvailable from "./json2Js.js";
-import Header from "./getHeader.js";
-import editNav from "./navResp.js";
-import flightMenu from "./flightMenu.js";
+import databaseAvailable from "./js/json2Js.js";
+import Header from "./js/getHeader.js";
+import editNav from "./js/navResp.js";
+import flightMenu from "./js/flightMenu.js";
 databaseAvailable.then(function(r){
         new Header().getHeader(r);
         new Header().getNavFilter(r);
