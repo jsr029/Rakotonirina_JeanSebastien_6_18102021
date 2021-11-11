a4be659e792546b7e3ec735ae094cf2b5e5b448f
jsr029 <jsr029@gmail.com>
2021-11-11 17:28:47 +0100

    Update

--- js/flightMenu.js
+++ js/flightMenu.js
@@ -3,7 +3,7 @@ class FlightMenu {
         const flm = document.getElementById("flightMenu");
         window.addEventListener("scroll", function () {
             const y = window.scrollY;
-            if (y >= 120 && flm.style.width > "174px") {
+            if (y >= 120 && screen.width > 840) {
                 flm.style.display = "block";
             } else {
                 flm.style.display = "none";
