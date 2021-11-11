e677fb0bcf8c05f75868fad9ad785f099c0131f1
jsr029 <jsr029@gmail.com>
2021-11-11 19:32:03 +0100

    Update

---
+++ js/GetPhs.js
@@ -0,0 +1,25 @@
+class GetPhs{
+    listPhs(dataPhotos){
+        dataPhotos.forEach(element => {
+        let section = document.querySelector('section');
+        const article = document.createElement('article');
+        article.className = "ph";
+        section.appendChild(article);
+           let boxProfil = `
+            <a class="photograph" href="./photograph.html?id=${element.id}" tabindex="${5}">
+                <img class="foto" src="./img/Photographers ID Photos/${element.portrait}" alt="${element.name}">
+                <h2>${element.name}</h2>
+            </a>
+            <ul class="locationTaglinePrice" tabindex="6">
+                <li>${element.city}, ${element.country}</li>
+                <li>${element.tagline}</li>
+                <li>${element.price}€/j</li>
+            </ul>
+            <ul class="tags" tabindex="7">${element.tags.map(tag =>
+                `<li data-filter="${tag}">#${tag}</li>`).join(" ")}</ul> 
+            `;
+            article.innerHTML = boxProfil;
+        });
+    }
+}
+export default GetPhs;
