27340a936a293e41567de860a1a6f01ae5a37283
jsr029 <jsr029@gmail.com>
2021-11-12 01:46:49 +0100

    getPhs.js issue

--- js/getPhs.js
+++ js/getPhs.js
@@ -6,16 +6,16 @@ class GetPhs{
         article.className = "ph";
         section.appendChild(article);
            let boxProfil = `
-            <a class="photograph tabIndex" href="./photograph.html?id=${element.id}">
+            <a class="photograph" href="./photograph.html?id=${element.id}" tabindex="${5}">
                 <img class="foto" src="./img/Photographers ID Photos/${element.portrait}" alt="${element.name}">
                 <h2>${element.name}</h2>
             </a>
-            <ul class="locationTaglinePrice tabIndex">
+            <ul class="locationTaglinePrice" tabindex="6">
                 <li>${element.city}, ${element.country}</li>
                 <li>${element.tagline}</li>
                 <li>${element.price}€/j</li>
             </ul>
-            <ul class="tags tabIndex">${element.tags.map(tag =>
+            <ul class="tags" tabindex="7">${element.tags.map(tag =>
                 `<li data-filter="${tag}">#${tag}</li>`).join(" ")}</ul> 
             `;
             article.innerHTML = boxProfil;
