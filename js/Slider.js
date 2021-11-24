ece9d55afb182366f9bd88f1431103640f3937b5
jsr029 <jsr029@gmail.com>
2021-11-24 16:44:28 +0100

    Update slider but issue

--- js/Slider.js
+++ js/Slider.js
@@ -1,15 +1,32 @@
 var SliderModal = function (pv, index) {
     var slide = pv;
-    console.log(slide[index]);
     //console.log(slide);
     var slideMoins = document.querySelector('#precedent');
     slideMoins.addEventListener('click', function (elt) {
-        ChangeSlide(slide, -1);
-    });
+        index--;
+        var slide = pv;
+        var numero = 0;
+        numero = numero + index;
+        if (numero < 0)
+            numero = slide.length - 1;
+        if (0 > numero > slide.length - 1)
+            numero = 0;
+        var slideModal = document.querySelector("#slide");
+        slideModal.innerHTML = slide[numero].innerHTML;
+        });
     var slidePlus = document.querySelector('#suivant');
     slidePlus.addEventListener('click', function (elt) {
-        ChangeSlide(slide, +1);
-    });
+        index++;
+        var slide = pv;
+        var numero = 0;
+        numero = numero + index;
+        if (numero < 0)
+            numero = slide.length - 1;
+        if (0 > numero > slide.length - 1)
+            numero = 0;
+        var slideModal = document.querySelector("#slide");
+        slideModal.innerHTML = slide[numero].innerHTML;
+        });
 };
 
 function ChangeSlide(pv, index) {
@@ -18,10 +35,9 @@ function ChangeSlide(pv, index) {
     numero = numero + index;
     if (numero < 0)
         numero = slide.length - 1;
-    if (numero > slide.length - 1)
+    if (0 > numero > slide.length - 1)
         numero = 0;
     var slideModal = document.querySelector("#slide");
-    console.log(slide[numero]);
     slideModal.innerHTML = slide[numero].innerHTML;
 }
 
