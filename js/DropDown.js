0ce6b8a6cee78bb565a966d10191979f20af4e3d
jsr029 <jsr029@gmail.com>
2021-12-02 14:21:45 +0100

    Update tri Css

--- js/DropDown.js
+++ js/DropDown.js
@@ -5,36 +5,57 @@ import AddLikes from "./addLikes.js";
 
 class DropDown {
     upDown(r) {
-        let dataMediaSort = [];
-        let dataMedia = r.media;
-        var allOptionSelect = document.querySelector('#tri-select');
-        allOptionSelect.addEventListener('click', function (opt) {
-            var index = allOptionSelect.selectedIndex;
-            if (index == 0) {
+            let dataMediaSort = [];
+            let dataMedia = r.media;
+            let pop = document.querySelector('#tri-select .popularite');
+            let dat = document.querySelector('#tri-select .date');
+            let tit = document.querySelector('#tri-select .titre');
+            let triArrow = document.querySelector('#tri-select i.triArrow');
+            let clicked = document.querySelector("#tri-select i.clicked");
+
+            pop.addEventListener('click', function (opt) {
                 console.log("Trier par : Popularité");
                 dataMediaSort = dataMedia.sort((a, b) => b.likes - a.likes);
-            } else if (index == 1) {
+                displayMediaSort(dataMediaSort);
+            });
+
+            dat.addEventListener('click', function (opt) {
                 console.log("Trier par : Date");
-                dataMediaSort = dataMedia.sort((a, b) => {  
+                dataMediaSort = dataMedia.sort((a, b) => {
                     return new Date(a.date).valueOf() - new Date(b.date).valueOf();
                 });
-            } else if (index == 2) {
+                displayMediaSort(dataMediaSort);
+            }); 
+
+            tit.addEventListener('click', function (opt) {
                 console.log("Trier par : titre");
                 dataMediaSort = dataMedia.sort(function (a, b) {
-                    if (a.title < b.title) {
-                        return 1;
-                    }
-                    if (a.title > b.title) {
-                        return -1;
+                if (a.title < b.title) {
+                    return 1;
+                }
+                if (a.title > b.title) {
+                    return -1;
                     }
                     return 0;
                 });
-            }
-            displayMediaSort(dataMediaSort);
-        });
+                displayMediaSort(dataMediaSort);
+            });
+            triArrow.addEventListener("click", function(event){
+                dat.style.display = 'block';
+                tit.style.display = 'block';
+                clicked.style.visibility = 'visible';
+                this.style.visibility = 'hidden';
+            });
+            clicked.addEventListener("click", function(event){
+                dat.style.display = 'none';
+                tit.style.display = 'none';
+                triArrow.style.visibility = 'visible';
+                this.style.visibility = 'hidden';
+            });
     }
 }
-    var displayMediaSort = function(dataMediaSort){
+                    
+    var displayMediaSort = function (dataMediaSort) {
         document.querySelector('#media').innerHTML = '';
         //new Photograph().getMediasByTags(dataMediaSort);
         var dataMedia = dataMediaSort;
@@ -52,35 +73,36 @@ class DropDown {
             var mediaExt = mediaSplit[1];
             if (d.photographerId == aClicked) {
                 var videoOrimg = (mediaExt === 'jpg') ?
-                    `<img src="./img/${idnSplit[0]}/${d.image}" alt="${d.title}, a ${idn.innerHTML}'s work">` :
-                    `<video controls><source src="./img/${idnSplit[0]}/${d.video}"></video>`;
+                `<img src="./img/${idnSplit[0]}/${d.image}" alt="${d.title}, a ${idn.innerHTML}'s work">` :
+                `<video controls><source src="./img/${idnSplit[0]}/${d.video}"></video>`;
                 let boxMedia = `
-                <article class="pictVideos">
-                <a href="javascript:void(0)">
-                    <div class="videoRimg">
-                        ${videoOrimg}
-                    </div>
-                    <div class="mediaDetails">
-                    <h2>${d.title}</h2>
-                        <span class="mediaPrice">${d.price}€</span>
-                        <span class="heartLikes"><i class="far fa-heart clicked" aria-hidden="true"></i></span>
-                    </div>
-                </a>
-                    <div class="mediaDetails">
-                    <h2>${d.title}</h2>
-                        <span class="mediaPrice">${d.price}€</span>
-                        <span class="mediaLikes">${d.likes}</span>
-                        <span class="heartLikes"><i class="far fa-heart clicked" aria-hidden="true"></i></span>
-                    </div>
-                </article>
-                    `;
-                mediaH.insertAdjacentHTML('afterbegin', boxMedia);
+                        <article class="pictVideos">
+                        <a href="javascript:void(0)">
+                            <div class="videoRimg">
+                                ${videoOrimg}
+                            </div>
+                            <div class="mediaDetails">
+                            <h2>${d.title}</h2>
+                                <span class="mediaPrice">${d.price}€</span>
+                                <span class="heartLikes"><i class="far fa-heart clicked" aria-hidden="true"></i></span>
+                            </div>
+                        </a>
+                            <div class="mediaDetails">
+                            <h2>${d.title}</h2>
+                                <span class="mediaPrice">${d.price}€</span>
+                                <span class="mediaLikes">${d.likes}</span>
+                                <span class="heartLikes"><i class="far fa-heart clicked" aria-hidden="true"></i></span>
+                            </div>
+                        </article>
+                            `;
+                        mediaH.insertAdjacentHTML('afterbegin', boxMedia);
             }
-     });
-        new Modal().showHtmlModal();
-        new Modal().addModal();
-        new Form().getFields();
-        new LightBox().addModal();
-        new AddLikes().adHeart();
-};
+            new Modal().showHtmlModal();
+            new Modal().addModal();
+            new Form().getFields();
+            new LightBox().addModal();
+            new AddLikes().adHeart();
+        });
+    };
+                    
 export default DropDown;
