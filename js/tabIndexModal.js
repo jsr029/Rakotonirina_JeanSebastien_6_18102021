90443edda35b29111d5ba879654346dd4af79f71
jsr029 <jsr029@gmail.com>
2021-12-15 11:56:36 +0100

    Update

--- js/tabIndexModal.js
+++ js/tabIndexModal.js
@@ -8,17 +8,17 @@ class TabIndexModal{
         let close = document.querySelector('.closeLightBox');
 
         var tabModalImgIndex = modalPict.children[0];
-        lightModal.setAttribute("tabindex", 8);
+        lightModal.setAttribute("tabindex", 14);
         lightModal.setAttribute("role", "dialog");
-        tabModalImgIndex.setAttribute("tabindex", 9);
+        tabModalImgIndex.setAttribute("tabindex", 14);
         tabModalImgIndex.setAttribute("role", "image");
-        h2.setAttribute("tabindex", 10);
+        h2.setAttribute("tabindex", 14);
         h2.setAttribute("role", "text");
-        precedent.setAttribute("tabindex", 11);
+        precedent.setAttribute("tabindex", 14);
         precedent.setAttribute("role", "link");
-        suivant.setAttribute("tabindex", 12);
+        suivant.setAttribute("tabindex", 14);
         suivant.setAttribute("role", "link");
-        close.setAttribute("tabindex", 13);    
+        close.setAttribute("tabindex", 14);    
         close.setAttribute("role", "button");
     }
 }
